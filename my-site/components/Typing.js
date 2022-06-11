22fede4f4a4a1ef5eba99ba6011b77e8be5e387d
chrispham0502 <trietpham0502@gmail.com>
2022-06-11 19:08:26 -0400

    Added arguments for Typing component

--- my-site/components/Typing.js
+++ my-site/components/Typing.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const Typing = ({staticText = "", deleteText = "", replaceText = ""}) => {
+const Typing = ({staticText = "", deleteText = "", replaceText = "" , curDelay, curCount, typingSpeed = 150, timeDelay = 2000}) => {
 
     const [phase, setPhase] = useState('PageLoad')
     const [typedText, setTypedText] = useState(deleteText)
@@ -10,14 +10,14 @@ const Typing = ({staticText = "", deleteText = "", replaceText = ""}) => {
             case 'PageLoad':
                 const timeout = setTimeout(() => {
                     setPhase('Deleting')
-                }, 2000)
+                }, timeDelay)
 
                 return () => clearTimeout(timeout)
 
             case 'Deleting': {
                 const timeout = setTimeout(() => {
                     setTypedText(deleteText.slice(0, typedText.length - 1))
-                }, 150)
+                }, typingSpeed)
                 
                 if (typedText == ""){
                     setPhase('Typing');
@@ -28,7 +28,7 @@ const Typing = ({staticText = "", deleteText = "", replaceText = ""}) => {
             case 'Typing': {
                 const timeout = setTimeout(() => {
                     setTypedText(replaceText.slice(0, typedText.length + 1))
-                }, 150)
+                }, typingSpeed)
 
                 return () => clearTimeout(timeout)
             }
@@ -39,7 +39,7 @@ const Typing = ({staticText = "", deleteText = "", replaceText = ""}) => {
     }, [typedText, phase])
 
   return (
-    <span class = "cursor"> {staticText} {typedText}</span>
+    <span class = "cursor" style={{"--blink-delay": curDelay, "--blink-count": curCount }}> {staticText} {typedText}</span>
   )
 }
 
