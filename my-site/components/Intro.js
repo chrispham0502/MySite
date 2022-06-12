ba0171f6e6267c3b33eaedbdedcdf7ff7fb872fc
chrispham0502 <trietpham0502@gmail.com>
2022-06-12 04:02:28 -0400

    Created typing speed variable and adjusted values

--- my-site/components/Intro.js
+++ my-site/components/Intro.js
@@ -2,23 +2,25 @@ import { useEffect, useState } from "react"
 import Typing from "./Typing"
 import styles from '../styles/Home.module.css'
 
+const typingSpeed = 100;
+
 const texts = [
-    <Typing key={1} staticText="Hello" deleteText=", World!" replaceText=" there!" typingSpeed={150} timeDelay={3500} />,
-    <Typing key={2} deleteText="Hello there!" replaceText="I'm Triet Pham" typingSpeed={150} />,
-    <Typing key={3} deleteText="I'm Triet Pham" replaceText="Welcome to my page!" typingSpeed={150} />
+    <Typing key={1} staticText=" Hello" deleteText=", World!" replaceText=" there!" typingSpeed={typingSpeed} timeDelay={3500} />,
+    <Typing key={2} deleteText=" Hello there!" replaceText="I'm Triet Pham" typingSpeed={typingSpeed} />,
+    <Typing key={3} deleteText=" I'm Triet Pham" replaceText="Welcome to my website!" curCount={9} typingSpeed={typingSpeed} />
 ]
 
 
 const Intro = () => {
-    const [phase, setPhase] = useState('New')
+    const [phase, setPhase] = useState('First')
     const [intro, setIntro] = useState(texts[0])
 
     useEffect(() => {
         switch (phase) {
-            case 'New':
+            case 'First':
                 const timeout = setTimeout(() => {
                     setPhase('Second')
-                }, 6000)
+                }, 5250)
 
                 return () => clearTimeout(timeout)
 
@@ -26,7 +28,7 @@ const Intro = () => {
                 setIntro(texts[1])
                 const timeout = setTimeout(() => {
                     setPhase('Final')
-                }, 6000)
+                }, 5000)
 
                 return () => clearTimeout(timeout)
             }
