d756019412c6406ac4813b3262d87ec343ac0e66
chrispham0502 <trietpham0502@gmail.com>
2022-06-13 19:04:51 -0400

    Added links to footer

--- my-site/components/Footer.js
+++ my-site/components/Footer.js
@@ -1,9 +1,26 @@
+import { AiFillGithub, AiOutlineLinkedin, AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
+
 
 const Footer = () => {
   return (
-        <footer>
-          Created by Triet Pham
-        </footer>
+    <>
+    <footer>
+        <div className="flex flex-row">
+          <a className="hover:opacity-50 ease-in-out duration-300" href="https://www.facebook.com/minhtriet.pham.0502" target="_blank" rel="noopener noreferrer">
+            <AiOutlineFacebook size={25}/>
+          </a>
+          <a className="hover:opacity-50 ease-in-out duration-300" href="https://www.instagram.com/snow.0502/" target="_blank" rel="noopener noreferrer">
+            <AiOutlineInstagram size={25}/>
+          </a>
+          <a className="hover:opacity-50 ease-in-out duration-300" href="https://www.linkedin.com/in/triet-pham-0502/" target="_blank" rel="noopener noreferrer">
+            <AiOutlineLinkedin size={25}/>
+          </a>
+          <a className="hover:opacity-50 ease-in-out duration-300" href="https://github.com/chrispham0502" target="_blank" rel="noopener noreferrer">
+            <AiFillGithub size={25} />
+          </a>
+        </div>
+      </footer>
+    </>
   )
 }
 
