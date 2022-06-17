b1aca9bbacb54723bb9681572f58c4ac96330f44
chrispham0502 <trietpham0502@gmail.com>
2022-06-17 03:08:13 -0400

    Added spaces in headers

--- my-site/pages/about.js
+++ my-site/pages/about.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import AboutContent from '../components/AboutContent'
 import Button from '../components/Button'
-import IconText from '../components/IconText'
 import Typing from '../components/Typing'
 
 
@@ -17,19 +16,19 @@ function About() {
         
           {/* ABOUT */}
           <h1 className="p-head mt-10">
-            <Typing key={1} replaceText="ABOUT ME" typingInterval={80} typeDelay={500} curCount={4} />
+            <Typing key={1} replaceText="&nbsp;ABOUT ME" typingInterval={80} typeDelay={500} curCount={4} />
           </h1>
           <AboutContent contentName="about" timeDelay={4000}/>
 
           {/* SKILLS */}
           <h1 className="p-head mt-16">
-            <Typing key={3} replaceText="MY SKILLS" typingInterval={80} timeDelay={17000} typeDelay={500} curCount={4} />
+            <Typing key={3} replaceText="&nbsp;MY SKILLS" typingInterval={80} timeDelay={17000} typeDelay={500} curCount={4} />
           </h1>
           <AboutContent contentName="skills" timeDelay={21000}/>
           
           {/* FUN FACTS */}
           <h1 className="p-head mt-16">
-            <Typing key={6} replaceText="FUN FACTS" typingInterval={80} timeDelay={38000} typeDelay={500} curCount={4} />
+            <Typing key={6} replaceText="&nbsp;FUN FACTS" typingInterval={80} timeDelay={38000} typeDelay={500} curCount={4} />
           </h1>
           <AboutContent contentName="facts" timeDelay={42000}/>
 
