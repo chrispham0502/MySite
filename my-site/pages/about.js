1681ff1fedf1a2faa3cecf4124b7feef2ff78fd9
chrispham0502 <trietpham0502@gmail.com>
2022-06-20 05:16:20 -0400

    Changed AboutContent to BoxContent

--- my-site/pages/about.js
+++ my-site/pages/about.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import AboutContent from '../components/AboutContent'
+import BoxContent from '../components/BoxContent'
 import Button from '../components/Button'
 import Typing from '../components/Typing'
 
@@ -18,19 +18,19 @@ function About() {
           <h1 className="p-head mt-10">
             <Typing key={1} replaceText="&nbsp;ABOUT ME" typingInterval={80} typeDelay={500} curCount={4} />
           </h1>
-          <AboutContent contentName="about" timeDelay={4000}/>
+          <BoxContent contentName="about" timeDelay={4000}/>
 
           {/* SKILLS */}
           <h1 className="p-head mt-16">
             <Typing key={3} replaceText="&nbsp;MY SKILLS" typingInterval={80} timeDelay={17000} typeDelay={500} curCount={4} />
           </h1>
-          <AboutContent contentName="skills" timeDelay={21000}/>
+          <BoxContent contentName="skills" timeDelay={21000}/>
           
           {/* FUN FACTS */}
           <h1 className="p-head mt-16">
             <Typing key={6} replaceText="&nbsp;FUN FACTS" typingInterval={80} timeDelay={38000} typeDelay={500} curCount={4} />
           </h1>
-          <AboutContent contentName="facts" timeDelay={42000}/>
+          <BoxContent contentName="facts" timeDelay={42000}/>
 
           {/* EXPERIENCE BUTTON */}
           <div className="my-10 sm:my-20 ">
